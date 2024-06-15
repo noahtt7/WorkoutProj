84dea0a5528ea1da9cf55a36399e6a77405c7b70
noahtt7 <noahtaguma@gmail.com>
2024-06-15 09:52:32 +0900

    Flexbox, useState, listcontainer style

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [exercises, setExercises] = useState([]);
+
   return (
     <View style={styles.appcontainer}>
       <Text>Open up App.js to start !</Text>
       <StatusBar style="auto" />
-      <TextInput 
-        style={styles.input}
-        placeholder='Enter exercise'/>
-        <FlatList>
+      <View style={styles.listcontainer}>
+        <TextInput 
+          style={styles.input}
+          placeholder='Enter exercise'/>
+        <FlatList
+        data={[
+          {key: 'Incline bench'},
+          {key: 'Squat'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>
+          {item.key}</Text>}
+        />
+        <Text height={300}>HIIIIIII</Text>
+      </View>
+        
           
-        </FlatList>
+        
     </View>
   );
 }
@@ -28,5 +42,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     color: '#120438',
     height: 40,
+  },
+  item: {
+    padding: 10,
+    height: 44
+  },
+  listcontainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between  '
   }
 });
