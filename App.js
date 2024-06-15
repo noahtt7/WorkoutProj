893d26a44a713d4364f8b9ce33f1dd6a663fb3f2
noahtt7 <noahtaguma@gmail.com>
2024-06-15 10:27:19 +0900

    Storing state: enteredExerciseText now can be added to exercises list

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList, Button } from 'react-native';
 
 export default function App() {
+  const [enteredExerciseText, setEnteredExerciseText] = useState('');
   const [exercises, setExercises] = useState([]);
+
+  function inputExerciseHandler(enteredText) {
+    // entered text in TextInput box is set
+    // to enteredExerciseText state
+    setEnteredExerciseText(enteredText);
+  };
+
+  function addExerciseHandler() {
+    // Add new enteredExerciseText to exercises array
+    console.log(enteredExerciseText);
+    setExercises(currentExercises => [...currentExercises, enteredExerciseText]);
+  };
 
   return (
     <View style={styles.appcontainer}>
-      <Text>Open up App.js to start !</Text>
       <StatusBar style="auto" />
-      <View style={styles.listcontainer}>
+      <View style={styles.inputContainer}>
         <TextInput 
           style={styles.input}
-          placeholder='Enter exercise'/>
+          placeholder='Enter exercise'
+          onChangeText={inputExerciseHandler}/>
+        <Button
+          onPress={addExerciseHandler}
+          title="Add exercise"
+          color="#841584"
+        />
         <FlatList
         data={[
           {key: 'Incline bench'},
@@ -23,9 +41,9 @@ export default function App() {
         />
         <Text height={300}>HIIIIIII</Text>
       </View>
-        
-          
-        
+      <View style={styles.exerciseContainer}>
+        <Text>List of exercises</Text>
+      </View>
     </View>
   );
 }
@@ -33,6 +51,8 @@ export default function App() {
 const styles = StyleSheet.create({
   appcontainer: {
     flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -41,15 +61,22 @@ const styles = StyleSheet.create({
     borderColor: '#e4d0ff',
     borderWidth: 1,
     color: '#120438',
+    width: '70%',
+    marginRight: 8,
     height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   item: {
     padding: 10,
     height: 44
   },
-  listcontainer: {
+  inputContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-between  '
+  },
+  exerciseContainer: {
+    flex: 4
   }
 });
